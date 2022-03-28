834bf94c720b387432af21ae7864ed8fb7ef9d5b
Patricia_Nalwanga <pmubiru2020@gmail.com>
2022-03-28 18:57:52 +0300

    temperature gauge assignment

---
+++ temperature.js
@@ -0,0 +1,12 @@
+console.log("Patricia");
+
+const temp = 38;
+if (temp >= 20 && temp <= 36) {
+  console.log("Your temperature is normal");
+}
+else if(temp >= 37 && temp <= 40){
+    console.log("Your temperature is above normal");
+}
+else(temp > 40){
+console.log("Seek medical help immediately!")}
+
