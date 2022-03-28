e1518fbdb367bd1529a879fc24082c8c67758850
Patricia_Nalwanga <pmubiru2020@gmail.com>
2022-03-28 19:03:59 +0300

    correct syntax error

--- temperature.js
+++ temperature.js
@@ -1,12 +1,8 @@
-console.log("Patricia");
-
 const temp = 38;
 if (temp >= 20 && temp <= 36) {
   console.log("Your temperature is normal");
+} else if (temp >= 37 && temp <= 40) {
+  console.log("Your temperature is above normal");
+} else {
+  console.log("Seek medical help immediately!");
 }
-else if(temp >= 37 && temp <= 40){
-    console.log("Your temperature is above normal");
-}
-else(temp > 40){
-console.log("Seek medical help immediately!")}
-
